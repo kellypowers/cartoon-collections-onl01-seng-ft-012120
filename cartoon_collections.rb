3358f83d3bb39efc4705b8fca55726353fa744f0
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 20:21:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,7 @@ end
 
 def find_the_cheese(array)
   #cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.find
+  array.each do |i|
 end
 
 cheese_types = ["cheddar", "gouda", "camembert"]
