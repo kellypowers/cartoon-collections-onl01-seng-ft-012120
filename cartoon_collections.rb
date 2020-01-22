eddf6e54fcf42a13c1345c675327681723e0df82
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 20:15:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  i = 0 
-  while i < array.length do 
-    array.include?(array[i].length > 4)
-    i += 1 
+    array.any?{|i| i.length > 4}
   end
 end
 
