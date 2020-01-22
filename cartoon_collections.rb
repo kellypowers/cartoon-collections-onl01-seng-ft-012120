0d3b927450c66c853311d35de5e50038dab4ba11
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 20:22:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  #cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |i|
 end
 
